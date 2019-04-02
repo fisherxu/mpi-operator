2b6ca35563122ae8b45568c5bb239fc90f5dc680
fisherxu <xufei40@huawei.com>
2019-04-02 16:39:42 +0800

    add leader election

--- cmd/mpi-operator/main.go
+++ cmd/mpi-operator/main.go
@@ -15,15 +15,26 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"os"
+	"time"
 
 	"github.com/golang/glog"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	restclientset "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	election "k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	"k8s.io/client-go/tools/record"
 	"k8s.io/sample-controller/pkg/signals"
 
+	"github.com/kubeflow/mpi-operator/pkg/apis/kubeflow/v1alpha1"
 	clientset "github.com/kubeflow/mpi-operator/pkg/client/clientset/versioned"
+	"github.com/kubeflow/mpi-operator/pkg/client/clientset/versioned/scheme"
 	informers "github.com/kubeflow/mpi-operator/pkg/client/informers/externalversions"
 	"github.com/kubeflow/mpi-operator/pkg/controllers"
 )
@@ -39,6 +50,13 @@ var (
 	enableGangScheduling   bool
 )
 
+const (
+	// leader election config
+	leaseDuration = 15 * time.Second
+	renewDuration = 5 * time.Second
+	retryPeriod   = 3 * time.Second
+)
+
 func main() {
 	flag.Parse()
 
@@ -50,14 +68,9 @@ func main() {
 		glog.Fatalf("Error building kubeConfig: %s", err.Error())
 	}
 
-	kubeClient, err := kubernetes.NewForConfig(cfg)
+	kubeClient, leaderElectionClientSet, kubeflowClient, err := createClientSets(cfg)
 	if err != nil {
-		glog.Fatalf("Error building kubernetes clientset: %s", err.Error())
-	}
-
-	kubeflowClient, err := clientset.NewForConfig(cfg)
-	if err != nil {
-		glog.Fatalf("Error building kubeflow clientset: %s", err.Error())
+		glog.Fatalf("Error building clientset: %s", err.Error())
 	}
 
 	var kubeInformerFactory kubeinformers.SharedInformerFactory
@@ -90,9 +103,67 @@ func main() {
 	go kubeInformerFactory.Start(stopCh)
 	go kubeflowInformerFactory.Start(stopCh)
 
-	if err = controller.Run(2, stopCh); err != nil {
-		glog.Fatalf("Error running controller: %s", err.Error())
+	// Set leader election start function.
+	run := func(ctx context.Context) {
+		if err = controller.Run(2, ctx.Done()); err != nil {
+			glog.Fatalf("Error running controller: %s", err.Error())
+		}
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		select {
+		case <-stopCh:
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+
+	id, err := os.Hostname()
+	if err != nil {
+		glog.Fatalf("failed to get hostname: %v", err)
+	}
+
+	// Prepare event clients.
+	eventBroadcaster := record.NewBroadcaster()
+	if err = v1.AddToScheme(scheme.Scheme); err != nil {
+		glog.Fatalf("CoreV1 Add Scheme failed: %v", err)
+	}
+	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: "mpi-operator"})
+
+	namespace := os.Getenv(v1alpha1.EnvKubeflowNamespace)
+	if len(namespace) == 0 {
+		glog.Infof("EnvKubeflowNamespace not set, use default namespace")
+		namespace = metav1.NamespaceDefault
+	}
+
+	rl := &resourcelock.EndpointsLock{
+		EndpointsMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+			Name:      "mpi-operator",
+		},
+		Client: leaderElectionClientSet.CoreV1(),
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity:      id,
+			EventRecorder: recorder,
+		},
+	}
+
+	// Start leader election.
+	election.RunOrDie(ctx, election.LeaderElectionConfig{
+		Lock:          rl,
+		LeaseDuration: leaseDuration,
+		RenewDeadline: renewDuration,
+		RetryPeriod:   retryPeriod,
+		Callbacks: election.LeaderCallbacks{
+			OnStartedLeading: run,
+			OnStoppedLeading: func() {
+				glog.Fatalf("leader election lost")
+			},
+		},
+	})
 }
 
 func init() {
@@ -112,4 +183,23 @@ func init() {
 		"(Deprecated. This will be overwritten by MPIJobSpec) The maximum number of processing units available per node. Note that this will be ignored if the processing resources are explicitly specified in the MPIJob pod spec.")
 	flag.StringVar(&processingResourceType, "processing-resource-type", "nvidia.com/gpu", "(Deprecated. This will be overwritten by MPIJobSpec) The compute resource name, e.g. 'nvidia.com/gpu' or 'cpu'.")
 	flag.BoolVar(&enableGangScheduling, "enable-gang-scheduling", false, "Whether to enable gang scheduling by kube-batch.")
+}
+
+func createClientSets(config *restclientset.Config) (kubernetes.Interface, kubernetes.Interface, clientset.Interface, error) {
+	kubeClientSet, err := kubernetes.NewForConfig(restclientset.AddUserAgent(config, "mpi-operator"))
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	leaderElectionClientSet, err := kubernetes.NewForConfig(restclientset.AddUserAgent(config, "leader-election"))
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	mpiJobClientSet, err := clientset.NewForConfig(config)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	return kubeClientSet, leaderElectionClientSet, mpiJobClientSet, nil
 }
